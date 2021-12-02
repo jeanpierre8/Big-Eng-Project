e966fc55ab6f5114fd97c44c507c9fbe091255ce
jeanpierre8 <jean-pierre.tran@efrei.net>
2021-12-02 10:26:25 -0800

    level up the base

--- app.py
+++ app.py
@@ -2,14 +2,33 @@ import time
 import redis
 from flask import Flask
 from flask import request
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 app=Flask(__name__)
-
-cache= redis.Redis(host='redis',port=6379)
+cache = redis.Redis(host='redis', port=6379)
 
 
 @app.route('/',methods=['GET'])
+def Sentence_print():
+    sentence = request.args.get('Sentence')
+    sentence = sentence.strip('-"][').replace('-',' ').split('.')
+    return "Your sentence is : {}".format(sentence)
+"""
+    obj = SentimentIntensityAnalyzer()
+    sentiment=obj.polarity_scores(sentence)
 
+    return ("sentence was rated as ", sentiment['neg']*100, "% Negative")
+
+    print("sentence was rated as ", sentiment['neu']*100, "% Neutral")
+    print("sentence was rated as ", sentiment['pos']*100, "% Positive")
+    print("")
+    if sentiment['compound']>=0.05:
+        print("The sentiment is Positive")
+    elif sentiment['compound']<= 0.05:
+        print("The sentiment is Negative")
+    else:
+        print("The sentiment is Neutral")
+"""
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
