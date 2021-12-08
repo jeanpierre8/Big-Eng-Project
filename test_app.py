7a04e737d28bf0bf35061a27c1e5331d547582f3
jeanpierre8 <jean-pierre.tran@efrei.net>
2021-12-07 23:57:10 -0800

    update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,9 +5,8 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import accuracy_score, f1_score,classification_report
 
-def test_index():
+def test_vader_model():
     df = pd.read_csv('moviereviews.tsv', sep ='\t')
-    df_table = df.head()
     df.dropna(inplace=True)
     blanks = []
     for i,lb,rv in df.itertuples():
@@ -20,3 +19,19 @@ def test_index():
     df['compound']= df['scores'].apply(lambda score: score['compound'])
     df['com_score']= df['compound'].apply(lambda c: 'pos' if c >=0 else 'neg')
     assert (accuracy_score(df['label'],df['com_score'])) > 80
+
+analyser = SentimentIntensityAnalyzer()
+sentence = 'this kitchen is delicious'
+sentence2 = 'this kitchen is DELICIOUS'
+sentence3 = 'this kitchen is delicious !!!!'
+sentence4 = 'this kitchen is DELICIOUS !!!!'
+text = analyser.polarity_scores(sentence)
+text2 = analyser.polarity_scores(sentence2)
+text3 = analyser.polarity_scores(sentence3)
+text4 = analyser.polarity_scores(sentence4)
+
+def test_accuracy_text_lower():
+    assert text['compound'] > text2['compound']
+
+def test_accuracy_text_pounctuation():
+    assert text['compound'] < text3['compound'] < text4['compound']
