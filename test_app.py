4f665dadab9e534622b48519e767efafb9027c58
loukili123 <bilal.loukili.pro@gmail.com>
2021-12-13 03:23:41 +0100

    adding integration tests

--- test_app.py
+++ test_app.py
@@ -1,9 +1,52 @@
 import pytest
+import requests
 import app
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import numpy as np
 import pandas as pd
 from sklearn.metrics import accuracy_score, f1_score,classification_report
+import json
+from bs4 import BeautifulSoup as BS
+
+def test_site_is_working():
+	assert requests.get('http://localhost:5000').status_code == 200
+
+
+def test_with_only_positive_words():
+	dataToSend = {'text': 'happy glad'}
+	x = requests.post('http://localhost:5000', data = dataToSend)
+	soup = BS(x.text, 'html.parser')
+	input_tag = soup.find(attrs={"id": "pos"}).getText()
+	output = float(input_tag)
+	print(output)
+	assert output == 100.00
+
+
+def test_with_only_negative_words():
+	dataToSend = {'text': 'sad destroying '}
+	x = requests.post('http://localhost:5000', data = dataToSend)
+	soup = BS(x.text, 'html.parser')
+	input_tag = soup.find(attrs={"id": "neg"}).getText()
+	output = float(input_tag)
+	print(output)
+	assert output == 100.00
+
+def test_with_only_neutral_words():
+	dataToSend = {'text': 'of with'}
+	x = requests.post('http://localhost:5000', data = dataToSend)
+	soup = BS(x.text, 'html.parser')
+	input_tag = soup.find(attrs={"id": "neut"}).getText()
+	output = float(input_tag)
+	print(output)
+	assert output == 100.00
+
+def test_site_can_handle_stress():
+	seconds = 0.0
+	for i in range(1000):
+		response = requests.get("http://localhost:5000")
+		seconds+=response.elapsed.total_seconds()
+	assert seconds < 100
+	
 
 def test_vader_model():
     df = pd.read_csv('moviereviews.tsv', sep ='\t')
@@ -35,4 +78,16 @@ def test_accuracy_text_lower():
 
 def test_accuracy_text_pounctuation():
     assert text['compound'] < text3['compound'] < text4['compound']
+
+
+
+
+
+
+
+
+
+
+
+
 
